eb2133ee1360cd8b08753fd3737eab0847231786
Roeut Chak <roeut.chak@gmail.com>
2018-01-05 02:20:32 -0500

    made comment changes to Program.cs file

--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+// roeut making change for git test
 namespace DinerMax3000Console
 {
     class MainClass
@@ -21,7 +21,7 @@ namespace DinerMax3000Console
                 MenuItem currentItem = summerMenu.items[x];
                 hungryGuestOrder.items.Add(currentItem);
             }
-
+//random comments 
             foreach(MenuItem currentItem in outsideDrinks.items){
                 hungryGuestOrder.items.Add(currentItem);
             }
